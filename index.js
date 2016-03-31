28d215f2ee5d1f9969d5d924ed28a7d0f797d9c8
ignatius123 <ignatius@projectcodex.co>
2016-03-31 13:11:27 +0200

    Not Done

---
+++ index.js
@@ -0,0 +1,21 @@
+var products = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50.";
+
+var number = [];
+
+var number = products.split(",");
+number.push();
+console.log(number);
+
+var newNumber = [];
+
+number.forEach(function(product){
+  var newNumber = number.split(" ");
+if(newNumber.indexOf(number) === 0);
+  newNumber.push(number);
+
+});
+
+
+for (var i = 0; i < number.length; i++){
+ if(number.indexOf)
+}
