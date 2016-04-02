de7d5113c97ca30460b6b64f0c566f0ed6804ed7
ignatius123 <ignatius@projectcodex.co>
2016-04-02 13:43:07 +0200

    Avos Done

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-var products = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50.";
+var products = "1 for R3,2 for R7, 3 for R10,5 for R14.50.";
 
-var number = [];
+if (products[products.length-1] === ".")
+  products = products.slice(0,-1);
 
-var number = products.split(",");
-number.push();
-console.log(number);
+var numbers = products.split(",");
 
-var newNumber = [];
+var deals = [];
 
-number.forEach(function(product){
-  var newNumber = number.split(" ");
-if(newNumber.indexOf(number) === 0);
-  newNumber.push(number);
+ numbers.forEach(function(products){
+deals.push(products.split("for R"));
 
-});
+ });
+
+var dealList = [];
+
+ deals.forEach(function(products){
+   var quantity = Number(products[0]);
+   var price =Number(products[1]);
+
+ var perAvoPrice = (price/quantity);
+
+   dealList.push(Number(perAvoPrice.toFixed(2)));
 
 
-for (var i = 0; i < number.length; i++){
- if(number.indexOf)
+ });
+
+var cheapestDeal = Math.min.apply(null, dealList);
+var expensiveDeal = Math.max.apply(null, dealList);
+
+
+var total = 0;
+
+for(var i = 0; i < dealList.length; i++){
+  total += dealList[i];
 }
+
+var averageDeal = Number((total/dealList.length).toFixed(2));
+
+console.log(dealList);
+console.log(cheapestDeal);
+console.log(expensiveDeal);
+console.log(averageDeal);
